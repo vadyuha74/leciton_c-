c5551114539d3def9fb2ecf404a979035487eded
vadim <vadik5719@yandex.ru>
2022-02-04 13:13:00 +0400

    Example_002

--- Example001_console/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.Write("введите имя");
-string username = Console.ReadLine();
-Console.Write("Привет");
-Console.Write(username);
---
+++ Example_003/Program.cs
@@ -0,0 +1,4 @@
+﻿int NumberA = 3;
+int NumberB = 5;
+int summ = NumberA + NumberB;
+Console.WriteLine(summ);
