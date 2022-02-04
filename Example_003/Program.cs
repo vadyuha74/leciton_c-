977ce795db056e39ca67bcde1437f23939291728
vadim <vadik5719@yandex.ru>
2022-02-04 15:58:07 +0400

    Random

--- Example_003/Program.cs
+++ Example_003/Program.cs
@@ -1,4 +1,6 @@
-﻿int NumberA = 3;
-int NumberB = 5;
+﻿int NumberA = new Random().Next(1,10);// случайное целое число из отрезка [1,10)
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1,10); // 1,2,3,...9
+Console.WriteLine(NumberB);
 int summ = NumberA + NumberB;
 Console.WriteLine(summ);
